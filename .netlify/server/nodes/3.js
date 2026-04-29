a444413f74222043311a7c7ecc57cc393847a314
johnstont05 <taylorfjohnston@gmail.com>
2026-04-29 15:55:44 -0400

    Remove build artifacts from tracking, add gitignore, fix OG image path
    Add proper .gitignore for SvelteKit/Netlify to stop tracking build/,
    .svelte-kit/, .netlify/, and .DS_Store files. Untrack all previously
    committed build artifacts. Fix SeoMeta og.png path to /img/og.png.

--- .netlify/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/about/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.CqF4F1tu.js","_app/immutable/chunks/scheduler.CbZlucOW.js","_app/immutable/chunks/index.DySwc99N.js"];
-export const stylesheets = ["_app/immutable/assets/3.uwJTbzBm.css"];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CwCcnKVR.js","_app/immutable/chunks/scheduler.CbZlucOW.js","_app/immutable/chunks/index.DySwc99N.js"];
-export const stylesheets = ["_app/immutable/assets/1.hbBo77CW.css"];
-export const fonts = [];
